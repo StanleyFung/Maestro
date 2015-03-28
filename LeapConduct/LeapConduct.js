0dfa7daa83fd55fe29e0ead8110c9ee137e3329f
StanleyFung <stanleyfung94@hotmail.com>
2015-03-28 15:37:42 -0400

    Myo and leap asynch

--- LeapConduct/LeapConduct.js
+++ LeapConduct/LeapConduct.js
@@ -1,161 +1,184 @@
 /**
  * Created by sfung on 2015-03-28.
  */
-var oldX = 0;
-var oldY = 0;
-var oldTime = 0;
-var isMovingRight = false
-var isMovingUp = false
-var PRECISION_DECIMAL = 3
-var movingLeft = false
-var movingRight = false
-var movingUp = false
-var movingDown = false
+$(document).ready(function () {
+    var oldX = 0;
+    var oldY = 0;
+    var oldTime = 0;
+    var isMovingRight = false
+    var isMovingUp = false
+    var PRECISION_DECIMAL = 3
+    var movingLeft = false
+    var movingRight = false
+    var movingUp = false
+    var movingDown = false
 
 //Stage of conducting is int from 0 - 3
-var DOWN = 0
-var LEFT = 1
-var RIGHT = 2
-var UP = 3
-var stageOfConducting = UP
+    var DOWN = 0
+    var LEFT = 1
+    var RIGHT = 2
+    var UP = 3
+    var stageOfConducting = UP
 
-window.output = $('#output');
-var movingLeftLabel = document.getElementById('movingLeft');
-var movingRightLabel = document.getElementById('movingRight');
-var movingUpLabel = document.getElementById('movingUp');
-var movingDownLabel = document.getElementById('movingDown');
-var bpmLabel = document.getElementById('bpm');
-var songBPM = 140;
+    window.output = $('#output');
+    var movingLeftLabel = document.getElementById('movingLeft');
+    var movingRightLabel = document.getElementById('movingRight');
+    var movingUpLabel = document.getElementById('movingUp');
+    var movingDownLabel = document.getElementById('movingDown');
+    var bpmLabel = document.getElementById('bpm');
+    var songBPM = 140;
 
-var velocityThreshold = 55;
+    var velocityThreshold = 55;
 
-var timeIntervals = [120, 120, 120, 120]
-var previousTime = 0;
+    var timeIntervals = [120, 120, 120, 120]
+    var previousTime = 0;
 
-var songStarted = false;
+    var songStarted = false;
 
-var pbAdj = new playbackAdjuster();
+    var pbAdj = new playbackAdjuster();
 
-Leap.loop({
-    hand: function (hand) {
-        var currentTime = new Date().getTime();
-        var screenPosition = hand.screenPosition(hand.palmPosition);
-        var currentX = screenPosition[0].toPrecision(PRECISION_DECIMAL);
-        var differenceX = currentX - oldX;
+    //Myo
+    var myoinput = document.getElementById('myoinput');
 
-        var currentY = screenPosition[1].toPrecision(PRECISION_DECIMAL);
-        var differenceY = currentY - oldY;
-
-        if (Math.abs(differenceX) > velocityThreshold) {
-            // console.log(differenceX)
-
-            if (isMovingRight && differenceX < 0) {
-                // console.log("Changed Direction Left")
-                movingLeft = true
-                movingRight = false
-            } else if (!isMovingRight && differenceX > 0) {
-                // console.log("Changed Direction Right")
-                movingRight = true
-                movingLeft = false
-            }
-
-            if (differenceX > 0) {
-                isMovingRight = true
-            } else if (differenceX < 0) {
-                isMovingRight = false
-            }
+    var myo = Myo.create();
+    myo.on('fist', function(edge){
+        //Edge is true if it's the start of the pose, false if it's the end of the pose
+        if(edge){
+            pbAdj.pausePlayBack()
         }
-        oldX = currentX;
+    });
 
-        if (Math.abs(differenceY) > velocityThreshold) {
+    myo.on('fingers_spread', function(){
+        pbAdj.play()
+    })
 
-            if (isMovingUp && differenceY < 0) {
-                // console.log("Changed Direction Up")
-                movingUp = true
-                movingDown = false
-            } else if (!isMovingUp && differenceY > 0) {
-                // console.log("Changed Direction Down")
-                movingUp = false
-                movingDown = true
-            }
+    myo.on('imu', function (data) {
+        var x = data.orientation.x.toPrecision(PRECISION_DECIMAL)
+        var y = data.orientation.y.toPrecision(PRECISION_DECIMAL)
+        var z = data.orientation.z.toPrecision(PRECISION_DECIMAL)
+        myoinput.innerHTML = "Orientation-- X: " + x + " Y: " + y + " Z: "+ z;
+    })
 
-            if (differenceY > 0) {
-                isMovingUp = true
+    //LEAP
+    Leap.loop({
+        hand: function (hand) {
+            var currentTime = new Date().getTime();
+            var screenPosition = hand.screenPosition(hand.palmPosition);
+            var currentX = screenPosition[0].toPrecision(PRECISION_DECIMAL);
+            var differenceX = currentX - oldX;
 
-            } else if (differenceY < 0) {
-                isMovingUp = false
-            }
-        }
-        oldY = currentY
+            var currentY = screenPosition[1].toPrecision(PRECISION_DECIMAL);
+            var differenceY = currentY - oldY;
 
-        var outputContent = "x: " + (screenPosition[0].toPrecision(PRECISION_DECIMAL)) + 'px' +
-            "        <br/>y: " + (screenPosition[1].toPrecision(PRECISION_DECIMAL)) + 'px' +
-            "        <br/>z: " + (screenPosition[2].toPrecision(PRECISION_DECIMAL)) + 'px';
+            if (Math.abs(differenceX) > velocityThreshold) {
+                // console.log(differenceX)
 
+                if (isMovingRight && differenceX < 0) {
+                    // console.log("Changed Direction Left")
+                    movingLeft = true
+                    movingRight = false
+                } else if (!isMovingRight && differenceX > 0) {
+                    // console.log("Changed Direction Right")
+                    movingRight = true
+                    movingLeft = false
+                }
 
-        output.html(outputContent);
-        movingLeftLabel.innerHTML = "Moving Left: " + movingLeft;
-        movingRightLabel.innerHTML = "Moving Right: " + movingRight;
-        movingUpLabel.innerHTML = "Moving Up: " + movingUp;
-        movingDownLabel.innerHTML = "Moving Down: " + movingDown;
-
-        //Conducting Loop
-
-        if (stageOfConducting == UP) {
-            if (movingDown) {
-                songStarted = true
-                stageOfConducting = DOWN
-                console.log("Moved to Down")
-
-                isMovingRight = false
-                isMovingUp = false
-
-                movingLeft = false
-                movingRight = false
-                movingUp = false
-                movingDown = false
-                oldX = 0;
-                oldY = 0;
-                timeIntervals[DOWN] = currentTime - previousTime
-                previousTime = currentTime
-            }
-        }
-        else if (stageOfConducting == DOWN) {
-            if (movingLeft) {
-                stageOfConducting += 1
-                console.log("Moved to Left")
-                timeIntervals[DOWN] = currentTime - previousTime
-                previousTime = currentTime
-            }
-        } else if (stageOfConducting == LEFT) {
-            if (movingRight) {
-                stageOfConducting += 1
-                console.log("Moved to Right")
-                timeIntervals[RIGHT] = currentTime - previousTime
-                previousTime = currentTime
-            }
-        } else if (stageOfConducting == RIGHT) {
-            if (movingLeft && movingUp) {
-                stageOfConducting += 1
-                console.log("Moved to Up")
-                timeIntervals[UP] = currentTime - previousTime
-                previousTime = currentTime
-                //Update BPM
-                if (songStarted) {
-                    var total = timeIntervals[DOWN] + timeIntervals[LEFT] + timeIntervals[RIGHT] + timeIntervals[UP];
-                    var average = total / 4
-                    var speed = 1000 / average * 60;
-                    bpmLabel.innerHTML = "BPM: " + speed;
-                    pbAdj.adjustSpeed(speed / songBPM);
-
+                if (differenceX > 0) {
+                    isMovingRight = true
+                } else if (differenceX < 0) {
+                    isMovingRight = false
                 }
             }
+            oldX = currentX;
+
+            if (Math.abs(differenceY) > velocityThreshold) {
+
+                if (isMovingUp && differenceY < 0) {
+                    // console.log("Changed Direction Up")
+                    movingUp = true
+                    movingDown = false
+                } else if (!isMovingUp && differenceY > 0) {
+                    // console.log("Changed Direction Down")
+                    movingUp = false
+                    movingDown = true
+                }
+
+                if (differenceY > 0) {
+                    isMovingUp = true
+
+                } else if (differenceY < 0) {
+                    isMovingUp = false
+                }
+            }
+            oldY = currentY
+
+            var outputContent = "x: " + (screenPosition[0].toPrecision(PRECISION_DECIMAL)) + 'px' +
+                "        <br/>y: " + (screenPosition[1].toPrecision(PRECISION_DECIMAL)) + 'px' +
+                "        <br/>z: " + (screenPosition[2].toPrecision(PRECISION_DECIMAL)) + 'px';
+
+
+            output.html(outputContent);
+            movingLeftLabel.innerHTML = "Moving Left: " + movingLeft;
+            movingRightLabel.innerHTML = "Moving Right: " + movingRight;
+            movingUpLabel.innerHTML = "Moving Up: " + movingUp;
+            movingDownLabel.innerHTML = "Moving Down: " + movingDown;
+
+            //Conducting Loop
+
+            if (stageOfConducting == UP) {
+                if (movingDown) {
+                    songStarted = true
+                    stageOfConducting = DOWN
+                    console.log("Moved to Down")
+
+                    isMovingRight = false
+                    isMovingUp = false
+
+                    movingLeft = false
+                    movingRight = false
+                    movingUp = false
+                    movingDown = false
+                    oldX = 0;
+                    oldY = 0;
+                    timeIntervals[DOWN] = currentTime - previousTime
+                    previousTime = currentTime
+                }
+            }
+            else if (stageOfConducting == DOWN) {
+                if (movingLeft) {
+                    stageOfConducting += 1
+                    console.log("Moved to Left")
+                    timeIntervals[DOWN] = currentTime - previousTime
+                    previousTime = currentTime
+                }
+            } else if (stageOfConducting == LEFT) {
+                if (movingRight) {
+                    stageOfConducting += 1
+                    console.log("Moved to Right")
+                    timeIntervals[RIGHT] = currentTime - previousTime
+                    previousTime = currentTime
+                }
+            } else if (stageOfConducting == RIGHT) {
+                if (movingLeft && movingUp) {
+                    stageOfConducting += 1
+                    console.log("Moved to Up")
+                    timeIntervals[UP] = currentTime - previousTime
+                    previousTime = currentTime
+                    //Update BPM
+                    if (songStarted) {
+                        var total = timeIntervals[DOWN] + timeIntervals[LEFT] + timeIntervals[RIGHT] + timeIntervals[UP];
+                        var average = total / 4
+                        var speed = 1000 / average * 60;
+                        bpmLabel.innerHTML = "BPM: " + speed;
+                        pbAdj.adjustSpeed(speed / songBPM);
+
+                    }
+                }
+            }
+            oldTime = currentTime;
         }
-        oldTime = currentTime;
-
-    }
+    })
+        .use('screenPosition', {
+            scale: 1
+        });
 })
-
-    .use('screenPosition', {
-        scale: 1
-    });
--- LeapConduct/musicLib.js
+++ LeapConduct/musicLib.js
@@ -1,15 +1,41 @@
+var SONG_ID = "songs"
+
 function playbackAdjuster() {
     this.adjustSpeed = function (factor) {
         console.log("Adjusting Speed by " + factor);
-        var song = document.getElementById("songs");
+        var song = document.getElementById(SONG_ID);
         song.playbackRate = factor;
         console.log("Current playback speed " + song.playbackRate)
     }
 
     this.adjustVolume = function (factor) {
         console.log("Adjusting Volume by " + factor);
-        var song = document.getElementById("songs");
+        var song = document.getElementById(SONG_ID);
         song.volume = factor + song.volume;
         console.log("Current playback speed " + song.volume)
+    }
+
+    this.pausePlayBack = function (){
+        var song = document.getElementById(SONG_ID);
+        song.pause();
+    }
+
+    this.stopPlayBack = function (){
+        var song = document.getElementById(SONG_ID);
+        song.pause();
+        song.src = '';
+        song.currentTime = 0;
+    }
+
+    this.play = function (){
+        var song = document.getElementById(SONG_ID);
+        song.play();
+    }
+    this.play = function (src){
+        var song = document.getElementById(SONG_ID);
+        song.src = src;
+    }
+    this.getSongObject = function(){
+        return document.getElementById(SONG_ID);
     }
 }
